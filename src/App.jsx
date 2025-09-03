7145418b3d34856020bfd448adf002977748c999
Muirama <mandresy.michel.ramandimbinirina@esti.mg>
2025-09-03 16:32:13 +0300

    UX and UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import NavBar from "./components/NavBar";
 import Carousel from "./components/Carousel";
 import Intro from "./components/Intro";
 import Footer from "./components/Footer";
+import Aurora from "./animations/Aurora";
 
 function App() {
   return (
     <>
+      <Aurora
+        colorStops={["#E50914", "#730b0b", "#1a1a1a"]}
+        amplitude={2}
+        blend={1}
+      />
       <NavBar />
       <Intro />
       <Carousel />
